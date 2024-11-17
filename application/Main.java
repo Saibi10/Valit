b77a2a182be8910265ae02941119c13e190e9cb0
Hussnainulabidin <hussnainulabidin707@gmail.com>
2024-11-17 17:24:44 +0500

    added

--- application/Main.java
+++ application/Main.java
@@ -1,5 +1,8 @@
 package application;
-	
+
+
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -10,7 +13,7 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage)  {
 		try {
 			//BorderPane root = new BorderPane();
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/Scenes/Admin.fxml"));
@@ -18,12 +21,18 @@ public class Main extends Application {
 			Scene scene = new Scene(root,1200,800);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+//			TourismManagementSystem TMS = new TourismManagementSystem();
 		} catch(Exception e) {
+			System.out.print("Error");
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+
+	public static void main(String[] args) throws SQLException 
+	{ 
+		launch(args); 
 	}
+	
 }
