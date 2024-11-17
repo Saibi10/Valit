70feb6964e3b5a27d7534607e134aec61c088685
Hussnainulabidin <hussnainulabidin707@gmail.com>
2024-11-17 14:34:07 +0500

    some admin work

---
+++ Query/SQLQuery2.sql
@@ -0,0 +1,6 @@
+SELECT TOP 3 u.FullName, COUNT(b.ID) AS NumberOfBookings, SUM(t.TourPrice) AS TotalAmountPaid
+FROM Booking b
+JOIN Users u ON b.UserID = u.UserID
+JOIN Tour t ON b.TourID = t.TourID
+GROUP BY u.FullName
+ORDER BY NumberOfBookings DESC, TotalAmountPaid DESC;
